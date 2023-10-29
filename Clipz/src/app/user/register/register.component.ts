6c4ef53091f97431a64c0b594772692ec6a1ffae
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-10-29 17:04:08 +0530

    Form Validation : Make a reusable input component and bind it with parent component fields. Add conditional validation messages

--- Clipz/src/app/user/register/register.component.ts
+++ Clipz/src/app/user/register/register.component.ts
@@ -7,16 +7,28 @@ import { FormGroup,FormControl,Validators } from '@angular/forms'
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  name = new FormControl('',[
+    Validators.required,
+    Validators.minLength(3)
+  ]);
+  email = new FormControl('',[
+    Validators.required,
+    Validators.email
+  ]);
+  age = new FormControl('',[
+    Validators.required,
+    Validators.min(18),
+    Validators.max(120)
+  ]);
+  password = new FormControl('');
+  confirm_password = new FormControl('');
+  phoneNumber = new FormControl('');
   registerForm = new FormGroup({
-      name: new FormControl('',[
-        Validators.required,
-        Validators.minLength(3)
-      ]),
-      email: new FormControl(''),
-      age: new FormControl(''),
-      password: new FormControl(''),
-      confirm_password: new FormControl(''),
-      phoneNumber: new FormControl(''),
+      name : this.name,
+      age: this.age,
+      password : this.password,
+      confirm_password:this.confirm_password,
+      phoneNumber:this.phoneNumber
 
   })
 }
