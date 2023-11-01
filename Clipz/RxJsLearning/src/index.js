2caac5af4ee41bbf01becdd2aaa4108bceb86e9b
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-11-02 00:14:51 +0530

    RxJs : Learn to use observable with async functions and clear memory leak.

---
+++ Clipz/RxJsLearning/src/index.js
@@ -0,0 +1,30 @@
+import { Observable } from "rxjs";
+
+const observable = new Observable((sub)=> {
+    const id = setInterval( () => {
+        sub.next('test');
+        console.log('leak');
+    },1000);
+
+    sub.complete();
+
+    return () => {
+    clearInterval(id);
+    }
+});
+
+
+
+observable.subscribe({
+    next: (value) => {
+       console.log(value)
+    },
+
+    complete: () => {
+        console.log("Complete");
+    },
+
+    error: (err) => {
+        console.error(err);
+    }
+})
