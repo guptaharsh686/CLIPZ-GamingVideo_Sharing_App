62e4a1aacbf505d9f7bd9647a3e37771515b9574
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-11-05 16:55:23 +0530

    Authentication : Create a public observable property for other components to check if the user is authenticated or not.

--- Clipz/src/app/services/auth.service.ts
+++ Clipz/src/app/services/auth.service.ts
@@ -2,21 +2,34 @@ import { Injectable } from '@angular/core';
 
 import {AngularFireAuth} from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { Observable, map } from 'rxjs';
 import IUser from 'src/app/Models/user.model'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  
+   //$ is special naming convention for identifying properties as observables.
+   public isAuthenticated$ : Observable<boolean>
+
+   private usersCollection : AngularFirestoreCollection<IUser>;
+   
   constructor(
     private auth : AngularFireAuth,
     private db : AngularFirestore
   ) {
     this.usersCollection = db.collection('users');
-   }
 
-  private usersCollection : AngularFirestoreCollection<IUser>
+    //auth.user.subscribe(console.log);
+    //use user observable provided by authentication service to verify if user is logged in
+    //if user obj is emitted by user observable then user is logged in if null is emitted then user is not logged in.
+    this.isAuthenticated$ =  auth.user.pipe(
+      map((user) => !!user)
+      )
+  }
 
   public async createUser(userData : IUser){
 
