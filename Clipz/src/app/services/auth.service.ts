35e6382870ee422832f511ea64987eac4d16007d
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-11-05 15:43:52 +0530

    Authentication : Refactor add Type safety to the collection.

--- Clipz/src/app/services/auth.service.ts
+++ Clipz/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 import {AngularFireAuth} from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import IUser from 'src/app/Models/user.model'
 
 @Injectable({
@@ -12,14 +12,22 @@ export class AuthService {
   constructor(
     private auth : AngularFireAuth,
     private db : AngularFirestore
-  ) { }
+  ) {
+    this.usersCollection = db.collection('users');
+   }
+
+  private usersCollection : AngularFirestoreCollection<IUser>
 
   public async createUser(userData : IUser){
+
+    if(!userData.password){
+      throw new Error("Password not provided!");
+    }
 
     const userCredential = await this.auth.createUserWithEmailAndPassword(userData.email,userData.password);
       // console.log(userCredential);
       //returns promise
-      await this.db.collection('users').add({
+      await this.usersCollection.add({
         name: userData.name,
         email: userData.email,
         age: userData.age,
