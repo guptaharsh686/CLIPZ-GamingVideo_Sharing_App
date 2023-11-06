4849dbe4bf3d39acc4b30426171a059b3540ff08
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-11-06 23:47:41 +0530

    Authentication : Implement the logout functionality

--- Clipz/src/app/nav/nav.component.ts
+++ Clipz/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,23 @@ import { AuthService } from '../services/auth.service';
 })
 export class NavComponent {
 
-  constructor(public modal : ModalService, public auth: AuthService){
+  constructor(
+    public modal : ModalService, 
+    public auth: AuthService,
+    public afAuth : AngularFireAuth
+    ){
   }
 
   openModal($event : Event){
     $event.preventDefault();
     this.modal.toggleModal('auth');
 
+  }
+
+  async Logout($event : Event){
+    $event.preventDefault();
+    //clear credintials from storage and revoke the token
+    await this.afAuth.signOut();
   }
 
 }
