8583b74e66e6603d867164033ca6d767ea0dc2a1
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-11-06 23:34:04 +0530

    Authentication : Destroying the model on successful authentication

--- Clipz/src/app/shared/modal/modal.component.ts
+++ Clipz/src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input , ElementRef} from '@angular/core';
+import { Component, Input , ElementRef,OnDestroy,OnInit} from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,14 +6,20 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent {
+export class ModalComponent implements OnDestroy,OnInit {
 
   @Input() modalID = ''
 
   constructor(public modal : ModalService, public el: ElementRef){
     //console.log(modal.visible); 
-    //console.log(el);
+    console.log(el);
+
+  }
+  ngOnInit(): void {
     document.body.appendChild(this.el.nativeElement);
+  }
+  ngOnDestroy(): void {
+    document.body.removeChild(this.el.nativeElement);
   }
 
   closeModal(){
