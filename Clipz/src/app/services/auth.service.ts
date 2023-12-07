66f1794fedab1257081b0ac3437d45ca456381a3
LAPTOP-RARL5FJK <guptaharsh686@gmail.com>
2023-12-08 00:00:50 +0530

    Routing : Modify routing logout behavior based on the data passed

--- Clipz/src/app/services/auth.service.ts
+++ Clipz/src/app/services/auth.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 
 import {AngularFireAuth} from '@angular/fire/compat/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
-import { Observable, delay, map } from 'rxjs';
+import { Observable, delay, map, filter, switchMap,of } from 'rxjs';
 import IUser from 'src/app/Models/user.model';
-
+import { ActivatedRoute,NavigationEnd } from '@angular/router';
 import { Router } from '@angular/router';
 
 
@@ -21,13 +21,15 @@ export class AuthService {
 
    private usersCollection : AngularFirestoreCollection<IUser>;
    
+   private redirect = false;
   constructor(
     private auth : AngularFireAuth,
     private db : AngularFirestore,
-    private router : Router
+    private router : Router,
+    private routeInfo : ActivatedRoute
   ) {
     this.usersCollection = db.collection('users');
-
+    //routeInfo.data.subscribe(console.log);
     //auth.user.subscribe(console.log);
     //use user observable provided by authentication service to verify if user is logged in
     //if user obj is emitted by user observable then user is logged in if null is emitted then user is not logged in.
@@ -38,6 +40,16 @@ export class AuthService {
       this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(
         delay(1000)
       )
+
+      this.router.events.pipe(
+        filter(e => e instanceof NavigationEnd),
+        map(e => this.routeInfo.firstChild),
+        switchMap(route => route?.data ?? of({
+          authOnly : null
+        }))
+      ).subscribe(data => {
+        this.redirect = data?.authOnly ?? false 
+      });
   }
  
   public async createUser(userData : IUser){
@@ -72,7 +84,9 @@ export class AuthService {
     await this.auth.signOut();
 
     //as method returns a promice we have to await it
-    await this.router.navigateByUrl('/');
+    if(this.redirect){
+      await this.router.navigateByUrl('/');
+    }
   }
 
 }
